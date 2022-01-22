6dc09ac2641ee2c6f4b7abc7eecb6218c98f6c9d
hsinawa <55555736+hsinawa@users.noreply.github.com>
2022-01-23 02:07:38 +0530

    Add files via upload

---
+++ client/src/loader.js
@@ -0,0 +1,18 @@
+import react from 'react'
+import './loader.css'
+
+const Loader=()=>{
+
+
+
+    return(
+        <div>
+<div class="loader" style={{textAlign:'center'}} ></div>
+        </div>
+    )
+
+
+}
+
+
+export default  Loader ;
